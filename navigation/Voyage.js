236b997bb14c1fd258ba25726498d918dee5fbdd
Leif Andreas Rudlang <leif.ross@gmail.com>
2017-02-14 23:18:51 +0100

    sync

---
+++ navigation/Voyage.js
@@ -0,0 +1,21 @@
+"use strict";
+
+/**
+* Class representing a collection of Routes
+* @author Leif Andreas Rudlang
+* @version 0.0.2
+* @since 0.0.2
+* @todo Refactor navigation api, a route is origin->destination, with offsets represented by waypoints in the form of routesegments
+*/
+class Voyage {
+
+  constructor() {
+
+  }
+
+
+}
+
+
+
+export default Voyage;
